eaf091247a8a416e9e1b5c6fa4f9c5b4ae35a349
dither <32025687+dither001@users.noreply.github.com>
2021-06-21 16:42:15 -0600

    gradle tweaks

--- desktop/src/com/worldview/game/desktop/DesktopLauncher.java
+++ desktop/src/com/worldview/game/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.worldview.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.worldview.game.WorldView;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new WorldView(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new WorldView(), config);
 	}
 }
