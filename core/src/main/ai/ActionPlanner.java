66fb79e2760ec72aa9e539c18c7af82808a0af98
Tanys Foster <dither001@hotmail.com>
2021-07-09 17:08:47 -0600

    ai - goap updates

--- core/src/main/ai/ActionPlanner.java
+++ core/src/main/ai/ActionPlanner.java
@@ -2,10 +2,14 @@ package main.ai;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import api.ai.IAction;
+import api.ai.ICondition;
 import api.ai.IGoal;
+import util.Util;
 
 public class ActionPlanner {
 
@@ -18,27 +22,65 @@ public class ActionPlanner {
 		return -1;
 	}
 
-	List<IAction> plan(IActor actor, Set<IAction> availableActions, IWorldState worldState, Set<IGoal> goals) {
+	public <K, V> List<IAction> plan(IActor actor, Set<IAction> available, Set<Entry<String, V>> state, Set<IGoal> goals) {
+		Util util = Util.getInstance();
 
 		// FIXME (needed?) reset actions for fresh start
-		availableActions.parallelStream().forEach(IAction::reset);
+		available.stream().forEach(IAction::reset);
 
 		// check what actions can run using their testConditions
-		Set<IAction> usableActions = Sets.newHashSet();
-		usableActions.stream().filter(IAction::testConditions).forEach(usableActions::add);
+		Set<IAction> viable = Sets.newHashSet();
+		viable.stream().filter(IAction::testConditions).forEach(viable::add);
 
 		List<Node> leaves = Lists.newArrayList();
 
-		return null;
+		// build graph
+		Node start = new Node(null, 0, state, null);
+		boolean success = buildGraph(start, leaves, viable, goals);
+
+		if (!success) {
+			return null; // failed to find plan
+		}
+
+		Node cheapest = null;
+		for (Node leaf : leaves) {
+			cheapest = util.ensure(cheapest) ? leaf.runningCost < cheapest.runningCost ? leaf : cheapest : leaf;
+		}
+
+		List<IAction> queue = Lists.newArrayList();
+		Node n = cheapest;
+		while (util.ensure(n)) {
+			if (util.ensure(n)) {
+				queue.add(0, n.action);
+			}
+			n = n.parent;
+		}
+
+		return queue;
+	}
+
+	private boolean buildGraph(Node parent, List<Node> leaves, Set<IAction> usable, Set<IGoal> goals) {
+		boolean foundOne = false;
+		
+//		usable.stream().filter(a -> testState(a.getConditions(), state))
+		
+		
+		
+		return foundOne;
+	}
+	
+	private boolean testState(ICondition[] conditions, IWorldState state) {
+		// TODO
+		return false;
 	}
 
 	private class Node {
 		public Node parent;
 		public float runningCost;
-		public IWorldState state;
+		public Set<Entry<K, V>> state;
 		public IAction action;
 
-		public Node(Node parent, float runningCost, IWorldState state, IAction action) {
+		public Node(Node parent, float runningCost, Set<Entry<K, V>> state, IAction action) {
 			this.parent = parent;
 			this.runningCost = runningCost;
 			this.state = state;
---
+++ core/src/main/ai/Test.java
@@ -0,0 +1,16 @@
+package main.ai;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+public class Test {
+
+	
+	
+	public void foo() {
+		CompletableFuture<Boolean> future = new CompletableFuture<>();
+		
+	}
+	
+	
+}
